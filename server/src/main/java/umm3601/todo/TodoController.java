3c58d13e013986649a1223fd465ca8efbcc06d33
Katelyn Money <money021@morris.umn.edu>
2026-01-22 13:28:54 -0600

    Got api/todos Working

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -33,6 +33,7 @@ import umm3601.Controller;
 //import umm3601.user.UserByCompany;
 
 public class TodoController implements Controller {
+
   private static final String API_TODO = "/api/todos";
   private static final String API_TODO_BY_ID = "/api/todos/{id}";
   static final String OWNER_KEY = "owner";
@@ -61,7 +62,7 @@ public class TodoController implements Controller {
       throw new BadRequestResponse("The requested user id wasn't a legal Mongo Object ID.");
     }
     if (todo == null) {
-      throw new NotFoundResponse("The requested user was not found");
+      throw new NotFoundResponse("The requested todo was not found");
     } else {
       ctx.json(todo);
       ctx.status(HttpStatus.OK);
@@ -69,16 +70,16 @@ public class TodoController implements Controller {
   }
 
 
-  public void getUsers(Context ctx) {
+  public void getTodos(Context ctx) {
     Bson combinedFilter = constructFilter(ctx);
     Bson sortingOrder = constructSortingOrder(ctx);
 
-    ArrayList<Todo> matchingUsers = todoCollection
+    ArrayList<Todo> matchingTodos = todoCollection
       .find(combinedFilter)
       .sort(sortingOrder)
       .into(new ArrayList<>());
 
-    ctx.json(matchingUsers);
+    ctx.json(matchingTodos);
 
     ctx.status(HttpStatus.OK);
   }
@@ -109,17 +110,15 @@ public class TodoController implements Controller {
 
     String body = ctx.body();
     Todo newTodo = ctx.bodyValidator(Todo.class)
-      .check(usr -> usr.owner != null && usr.owner.length() > 0,
-        "User must have a non-empty user name; body was " + body)
-      .check(usr -> usr.body != null && usr.body.length() > 0,
-        "User must have a non-empty user name; body was " + body)
-      .check(usr -> usr.category != null && usr.category.length() > 0,
-        "User must have a non-empty user name; body was " + body)
-      .check(usr -> usr.status,
-        "User must have a non-empty user name; body was " + body)
+      .check(td -> td.owner != null && td.owner.length() > 0,
+        "Todo must have a non-empty owner name; body was " + body)
+      .check(td -> td.body != null && td.body.length() > 0,
+        "Todo must have a non-empty body name; body was " + body)
+      .check(td -> td.category != null && td.category.length() > 0,
+        "Todo must have a non-empty category name; body was " + body)
+      .check(td -> td.status, "Todo must have a status.")
       .get();
 
-    // Add the new user to the database
     todoCollection.insertOne(newTodo);
 
     // Set the JSON response to be the `_id` of the newly created user.
@@ -134,15 +133,11 @@ public class TodoController implements Controller {
     ctx.status(HttpStatus.CREATED);
   }
 
-  /**
-   * Delete the user specified by the `id` parameter in the request.
-   *
-   * @param ctx a Javalin HTTP context
-   */
-  public void deleteUser(Context ctx) {
+
+  public void deleteTodo(Context ctx) {
     String id = ctx.pathParam("id");
     DeleteResult deleteResult = todoCollection.deleteOne(eq("_id", new ObjectId(id)));
-    // We should have deleted 1 or 0 users, depending on whether `id` is a valid user ID.
+
     if (deleteResult.getDeletedCount() != 1) {
       ctx.status(HttpStatus.NOT_FOUND);
       throw new NotFoundResponse(
@@ -153,36 +148,19 @@ public class TodoController implements Controller {
     ctx.status(HttpStatus.OK);
   }
 
-  /**
-   * Utility function to generate an URI that points
-   * at a unique avatar image based on a user's email.
-   *
-   * This uses the service provided by gravatar.com; there
-   * are numerous other similar services that one could
-   * use if one wished.
-   *
-   * YOU DON'T NEED TO USE THIS FUNCTION FOR THE TODOS.
-   *
-   * @param email the email to generate an avatar for
-   * @return a URI pointing to an avatar image
-   */
-  String generateAvatar(String email) {
-    String avatar;
-    try {
-      // generate unique md5 code for identicon
-      avatar = "https://gravatar.com/avatar/" + md5(email) + "?d=identicon";
-    } catch (NoSuchAlgorithmException ignored) {
-      // set to mystery person
-      avatar = "https://gravatar.com/avatar/?d=mp";
-    }
-    return avatar;
-  }
+  // String generateAvatar(String email) {
+  //   String avatar;
+  //   try {
+  //     // generate unique md5 code for identicon
+  //     avatar = "https://gravatar.com/avatar/" + md5(email) + "?d=identicon";
+  //   } catch (NoSuchAlgorithmException ignored) {
+  //     // set to mystery person
+  //     avatar = "https://gravatar.com/avatar/?d=mp";
+  //   }
+  //   return avatar;
+  // }
 
-  /**
-   * Utility function to generate the md5 hash for a given string
-   *
-   * @param str the string to generate a md5 for
-   */
+
   public String md5(String str) throws NoSuchAlgorithmException {
     MessageDigest md = MessageDigest.getInstance("MD5");
     byte[] hashInBytes = md.digest(str.toLowerCase().getBytes(StandardCharsets.UTF_8));
@@ -194,48 +172,16 @@ public class TodoController implements Controller {
     return result.toString();
   }
 
-  /**
-   * Sets up routes for the `user` collection endpoints.
-   * A UserController instance handles the user endpoints,
-   * and the addRoutes method adds the routes to this controller.
-   *
-   * These endpoints are:
-   *   - `GET /api/users/:id`
-   *       - Get the specified user
-   *   - `GET /api/users?age=NUMBER&company=STRING&name=STRING`
-   *      - List users, filtered using query parameters
-   *      - `age`, `company`, and `name` are optional query parameters
-   *   - `GET /api/usersByCompany`
-   *     - Get user names and IDs, possibly filtered, grouped by company
-   *   - `DELETE /api/users/:id`
-   *      - Delete the specified user
-   *   - `POST /api/users`
-   *      - Create a new user
-   *      - The user info is in the JSON body of the HTTP request
-   *
-   * GROUPS SHOULD CREATE THEIR OWN CONTROLLERS THAT IMPLEMENT THE
-   * `Controller` INTERFACE FOR WHATEVER DATA THEY'RE WORKING WITH.
-   * You'll then implement the `addRoutes` method for that controller,
-   * which will set up the routes for that data. The `Server#setupRoutes`
-   * method will then call `addRoutes` for each controller, which will
-   * add the routes for that controller's data.
-   *
-   * @param server The Javalin server instance
-   */
   @Override
   public void addRoutes(Javalin server) {
-    // Get the specified user
+
     server.get(API_TODO_BY_ID, this::getTodo);
 
-    // List users, filtered using query parameters
-    server.get(API_TODO, this::getTodo);
+    server.get(API_TODO, this::getTodos);
 
-    // Add new user with the user info being in the JSON body
-    // of the HTTP request
     server.post(API_TODO, this::addNewTodo);
 
-    // Delete the specified user
-    server.delete(API_TODO_BY_ID, this::deleteUser);
+    server.delete(API_TODO_BY_ID, this::deleteTodo);
   }
 
 }
